61ffe551ba1fe3012e92731392c4afcdc7e37409
J0nathan-CR <jacr-work@outlook.com>
2025-07-18 21:07:30 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let domain = ['.com', '.es', '.net'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomDomains = generateRandomDomains(8)
+
+  function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  function generateRandomDomains(howMany) {
+    let domains = [];
+
+    for (let i = 0; i < howMany; i++) {
+      let pronounRandom = getRandomElement(pronoun);
+      let adjRandom = getRandomElement(adj);
+      let nounRandom = getRandomElement(noun);
+      let domainRandom = getRandomElement(domain);
+
+      domains.push(pronounRandom + adjRandom + nounRandom + domainRandom);
+    }
+
+    return domains;
+  }
+
+  document.getElementById("content").innerHTML = randomDomains;
+  document.getElementById("reloadBtn").addEventListener("click", function () {
+  location.reload();
+});
+
+  console.log(randomDomains);
 };
